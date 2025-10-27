88e438db83807a18d5ec79b0ddafee7c775105f9
museongchoi <antjd3303@hs.ac.kr>
2025-10-27 19:35:42 +0900

    [refactor] : TCPClient/TCPServer AF, PF 수정 + TCPClient ServerSocket -> ClientSocket 변경

--- WindowSocketAPI/TCPClient/TCPClient.cpp
+++ WindowSocketAPI/TCPClient/TCPClient.cpp
@@ -12,28 +12,24 @@ int main()
 {
 	// [1] 파일 읽기
 	FILE* InputFile = fopen("../Assets/pororo.png", "rb");
-	if (!InputFile)
-	{
-		perror("파일 열기 실패");
-		return 1;
-	}
 
 	// [2] WinSock 초기화
 	WSAData wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	// [3] 서버 연결 소켓 생성
-	SOCKET ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+	// [3] 서버에 연결할 클라이언트 소켓 생성
+	SOCKET ClientSocket = socket(PF_INET, SOCK_STREAM, 0);
 
+	// 서버 소켓 주소를 저장할 구조체
 	struct sockaddr_in ServerSockAddr;
 	ZeroMemory(&ServerSockAddr, 0);
-	ServerSockAddr.sin_family = PF_INET;
+	ServerSockAddr.sin_family = AF_INET;
 	//ListenSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	inet_pton(PF_INET, "127.0.0.1", (void*)&(ServerSockAddr.sin_addr.s_addr));
+	inet_pton(AF_INET, "127.0.0.1", (void*)&(ServerSockAddr.sin_addr.s_addr));
 	ServerSockAddr.sin_port = htons(30000);
 
 	// [4] 서버 연결
-	connect(ServerSocket, (struct sockaddr*)&ServerSockAddr, sizeof(ServerSockAddr));
+	connect(ClientSocket, (struct sockaddr*)&ServerSockAddr, sizeof(ServerSockAddr));
 
 	// [5] 파일 전송
 	char Buffer[4096] = { 0, };
@@ -43,16 +39,16 @@ int main()
 	do
 	{
 		ReadSize = fread(Buffer, sizeof(char), sizeof(Buffer), InputFile);
-		SendBytes = send(ServerSocket, Buffer, (int)ReadSize, 0);
+		SendBytes = send(ClientSocket, Buffer, (int)ReadSize, 0);
 	} while (ReadSize > 0);
 
 
 	// [6] 종료
 	fclose(InputFile);
-	closesocket(ServerSocket);
+	closesocket(ClientSocket);
 	WSACleanup();
 
-	cout << "파일 전송 완료\n";
+	std::cout << "파일 전송 완료\n";
 
 	return 0;
 }
--- WindowSocketAPI/TCPServer/TCPServer.cpp
+++ WindowSocketAPI/TCPServer/TCPServer.cpp
@@ -10,29 +10,38 @@ using namespace std;
 
 int main()
 {
+	// [1] 파일 쓰기 : 수신 데이터를 저장할 파일 스트림 생성
 	FILE* OutputFile = fopen("../Assets/pororo_copy.png", "wb");
 
+	// [2] WinSock 초기화
 	WSADATA wsaData;
 	WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-	SOCKET ListenSocket = socket(AF_INET, SOCK_STREAM, 0);
+	// [3] 리슨 전용 소켓 생성
+	SOCKET ListenSocket = socket(PF_INET, SOCK_STREAM, 0);
 
+	// 리슨 소켓 주소 구조체
 	struct sockaddr_in ListenSockAddr;
 	ZeroMemory(&ListenSockAddr, 0);
-	ListenSockAddr.sin_family = PF_INET;
+	ListenSockAddr.sin_family = AF_INET;
 	//ListenSockAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	inet_pton(PF_INET, "127.0.0.1", (void*)&(ListenSockAddr.sin_addr.s_addr));
+	inet_pton(AF_INET, "127.0.0.1", (void*)&(ListenSockAddr.sin_addr.s_addr));
 	ListenSockAddr.sin_port = htons(30000);
 
+	// [4] 리슨 소켓 포트 바인딩
 	bind(ListenSocket, (struct sockaddr*)&ListenSockAddr, sizeof(ListenSockAddr));
 
+	// [5] 대기 상태 전환
 	listen(ListenSocket, 0);
 
+	// 클라이언트 소켓 주소를 저장할 구조체
 	struct sockaddr ClientSockAddr;
 	ZeroMemory(&ClientSockAddr, 0);
 	int ClientSockAddrLength = sizeof(ClientSockAddr);
+	// [6] 클라이언트 연결 요청 수락
 	SOCKET ClientSocket = accept(ListenSocket, (SOCKADDR*)&ClientSockAddr, &ClientSockAddrLength);
 
+	// [7] 데이터 수신
 	char Buffer[4096] = { 0, };
 	size_t WriteSize = 0;
 	int RecvByte = 0;
@@ -43,7 +52,10 @@ int main()
 		WriteSize = fwrite(Buffer, sizeof(char), RecvByte, OutputFile);
 	} while (RecvByte > 0);
 
+	// [8] 종료
 	fclose(OutputFile);
+
+	closesocket(ClientSocket);
 	closesocket(ListenSocket);
 	WSACleanup();
 
